133ca9c52bcda4aa85413b62ab2a932cd14bb369
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-05 22:12:15 -0500

    high score feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -126,23 +126,35 @@ var startGame = function(){
 };
 
 // function to end entire game
-var endGame = function(){
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    }
-    else {
-      window.alert("You've lost your robot in battle.");
-    }
-    var playAgainConfirm = window.confirm("Would you like to play again?");
+var endGame = function() {
+  window.alert("The game has now ended. Let's see how you did!");
 
-    if (playAgainConfirm) {
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player have more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
 
-      startGame();
-    }
-    else {
-      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
-    }
-}
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    startGame();
+  } 
+  else {
+    window.alert("Thank you for playing Battlebots! Come back soon!");
+  }
+};
 
 var shop = function(){
   var shopOptionPrompt = window.prompt(
